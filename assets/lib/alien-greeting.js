d389975b3ba082283d1f7219fd84d75f9cab7820
petrero <petrero@gmail.com>
2023-12-17 00:07:55 +0200

    5.4 Lazy-Loading CSS  - Here's where things get really spooky-cool. JavaScript modules have a dynamic import syntax that allows you to import modules asynchronously. That lets us load a file later when we need it, instead of on page load. And we can use this trick with CSS.
     Copy this. Pretend that we only want to load that CSS file when inPeace equals false. So I'll say, if not inPeace, then use setTimeout() to wait for 4 seconds. After 4 seconds, import the CSS file. Except, as soon as you need an import to not live at the top of your file, you need to call it like a function.
    
     That's pretty cool. Try it. At first, blue background! 2, 3, 4, green background! The CSS file loaded lazily. How? Well, there's no alien-greeting.css link tag in the page source anymore. Instead, we wait for the browser to execute this JavaScript line. When it does, it looks for this in the importmap, finds it and downloads this fake file. But this time, instead of it being a line that does nothing, this fake file adds a new link tag to the head section with rel="stylesheet" and href set to alien-greeting.css.
    
     Heck, we can watch this in real time! Over here, under the head tag, we see the stylesheet. If I refresh and quickly open that, it's not there. And... then it gets added. So stinkin' cool.
    
     Now that we've conquered how CSS works, tomorrow, we'll use it to bring our site to life! But I want to do it with an extra fun angle: I want to use Tailwind CSS.

--- assets/lib/alien-greeting.js
+++ assets/lib/alien-greeting.js
@@ -1,5 +1,8 @@
-import '../styles/alien-greeting.css';
-
 export  default function (message, inPeace = false) {
+  if (!inPeace) {
+    setTimeout(() => {
+      import('../styles/alien-greeting.css');
+    }, 4000);
+  }
   console.log(`${message}! ${inPeace ? '👽' : '👾'}`);
 }
