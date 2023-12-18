7ee1c1fe310c976f048aef95dd1c5fe2bbee98a7
petrero <petrero@gmail.com>
2023-12-18 21:30:57 +0200

    11.12 Remembering the Ajax Call  - Do we have time for one more thing? When we hover over the element, it makes the AJAX call. And... it repeats that every time we hover. It doesn't remember the content from the AJAX call.
     That's due to how the popover controller works... and if I had been less stubborn and used its way of Ajax-loading content, it wouldn't be a problem. Anyway, each time we hover, it creates the turbo-frame, destroys it, creates a new one, destroys it, etc.
    
     So: how can we make the controller remember the content? I have no idea! But let's go look inside the code. In assets/vendor/stimulus-popover/, open this file. The contents are minified... but a quick Cmd+L to reformat the code fixes that. How cool is this? We can now read this vendor file - and even add temporary debugging code if we needed to. And... I think I see a way that we can make this work.
    
     Just like with Symfony controllers, we can override Stimulus controllers. Inside the controllers/ directory, create our own popover_controller.js. Then I'll paste in some code
    
     Normally we import Controller from Stimulus and extend that. But in this case, I'm importing the popover controller directly and extending that. Then we override the show method and hide method to toggle a hidden class instead of fully destroying the element.
    
     And now that we have our own controller named popover, in bootstrap.js, we don't need to register the one from Stimulus components. The popover controller will be our controller... then we leverage the Stimulus components controller via inheritance.
    
     Moment of truth! It loads once... then remembers its content!
    
     Not only did we create the perfect popover, we can now easily repeat this on other parts of our site. If you're wondering if we could reuse some of the popover markup... stay tuned for Day 23 when we talk about Twig Components.
    
     That's a wrap for today! Get some well-deserved rest, because tomorrow we'll write a tiny, yet mighty, Stimulus controller called auto-submit.

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
-import Popover from 'stimulus-popover'
 
 const app = startStimulusApp();
-app.register('popover', Popover)
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
