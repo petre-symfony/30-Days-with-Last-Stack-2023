8351d74ba70480bc892312805ec54c7a2e0b0bea
petrero <petrero@gmail.com>
2023-12-18 17:25:12 +0200

    9.2 Turbo: What's the Catch?  - You might be thinking:
      This is too good to be true, Ryan. What's the catch?
    
     Ok, there is a catch, but minor for new projects: your JavaScript must be written to work without full page refreshes. Historically, we've written our JavaScript to execute on page load... or run on document.ready. And those things just don't happen after the first page load. But as long as you have everything written in Stimulus, you're good.
    
     For example: our celebrate controller: it doesn't matter how many pages I click around to, that just keeps on rolling.
    
     If your app isn't ready for Turbo yet - because of the JavaScript problem - you can disable it. In app.js, import * as Turbo from '@hotwired/turbo'. Below, say Turbo.session.drive = false. I'm not going to do that... so I'll comment it out

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 import './bootstrap.js';
 import './styles/app.css'
 import alienGreeting from './lib/alien-greeting.js';
+import * as Turbo from '@hotwired/turbo';
 
 alienGreeting('Give us all your candy!', false);
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// Turbo.session.drive = false;
