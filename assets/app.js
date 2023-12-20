36a6b16452c5dfeb951c5616f8374581e41a4a12
petrero <petrero@gmail.com>
2023-12-20 20:28:27 +0200

    15.2 Lovely! To activate transitions, we need to do two things. First, in base.html.twig, add a meta tag with name="view-transition"
     That's how you tell your browser you want these!
    
     Second, in Turbo 7, we need to activate transitions in JavaScript. Head to app.js. This will be a rare time when we write JavaScript that doesn't need to live in a Stimulus controller. Copy from their example, paste... and move the import to the top
    
     Done! That should be enough to make the Turbo Drive navigations use view transitions! This leverages an event from Turbo called turbo:before-render. The shouldPerformTransition() function checks to see if the user's browser supports transitions. If they don't, it's normal behavior. But if it does, then performTransition() will transition between the old and new body. There's also a little code down here to avoid the turbo page cache. I think that's something that'll work better in Turbo 8 when this is official.
    
     Time for the big reveal! Hit refresh and watch. Oooooh. A smooth crossfade transition! So not only did we eliminate full page reloads, we now have a transition between our pages. Watch out Powerpoint, we're coming for you!

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,24 @@
 import './bootstrap.js';
 import './styles/app.css'
 import alienGreeting from './lib/alien-greeting.js';
-import * as Turbo from '@hotwired/turbo';
+//import * as Turbo from '@hotwired/turbo';
+import { shouldPerformTransition, performTransition } from "turbo-view-transitions";
 
 alienGreeting('Give us all your candy!', false);
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 // Turbo.session.drive = false;
+
+document.addEventListener("turbo:before-render", (event) => {
+  if (shouldPerformTransition()) {
+    event.preventDefault();
+
+    performTransition(document.body, event.detail.newBody, async () => {
+      await event.detail.resume();
+    });
+  }
+});
+
+document.addEventListener("turbo:load", () => {
+  // View Transitions don't play nicely with Turbo cache
+  if (shouldPerformTransition()) Turbo.cache.exemptPageFromCache();
+});
