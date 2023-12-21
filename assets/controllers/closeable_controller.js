e7a64673aab633f409491a13d7040d6ce17e48a7
petrero <petrero@gmail.com>
2023-12-21 19:32:20 +0200

    17.1 Fancier Toasts: Auto-close & Fading
     Yesterday, we cooked up a beautiful Toast notification system that's powered entirely with CSS and Symfony's normal flash system. Ok, and just a tiny bit of JavaScript to, boop, close it.
    
     Today we're going to take this to the next level. I want these toasts to be amazing.
    
     Adding Auto-Close
     - The first feature we'll add is auto-close: a classic in the toast world where the message graces our screen, then closes automatically after a few seconds. But I also want to keep our closeable controller reusable. There may be other parts of the site where we want to be able to close something... but not have it close itself automatically.
    
     So, we need a way to activate the auto-close on a case-by-case basis. The way to pass info into a controller is via values. Add static values equals... and I'll invent a new one called autoClose, which will be a Number
    
     Next, add a connect() method. The idea is that if we have this.autoCloseValue - that's how you reference that - then... that's actually perfect! We'll use setTimeout to close after that many milliseconds
    
     To finish, go to where we use this controller - _flashes.html.twig - to pass in the new autoClose value. We do that on the same element as the data-controller. Add data-closeable-auto-close-value equals and use 5,000 for 5 seconds
    
     The format is data- the name of the controller, auto-close - that's the name of the value autoClose... but because we're in an HTML attribute, we use the "dash case" - then the word value equals and finally what we want to pass in. This format is harder to remember than just data-controller. But as you saw, if you have this Stimulus plugin for PhpStorm, it auto-completes it, which helps a lot.
    
     Let's do this! Edit this record, save and 1, 2, 3, 4, 5... whoosh! It vanishes.

--- assets/controllers/closeable_controller.js
+++ assets/controllers/closeable_controller.js
@@ -2,6 +2,17 @@ import {Controller} from '@hotwired/stimulus';
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
+  static values = {
+    autoClose: Number
+  }
+  connect() {
+    if (this.autoCloseValue) {
+      setTimeout(() => {
+        this.close()
+      }, this.autoCloseValue)
+    }
+  }
+
   close() {
     this.element.remove();
   }
