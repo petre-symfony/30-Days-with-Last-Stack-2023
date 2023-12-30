b15de369f941b8d53d62e3724dec1213108efda1
petrero <petrero@gmail.com>
2023-12-30 15:46:19 +0200

    21.2 Adding action Attributes to the Forms  - Here's the problem: when I generated the voyage crud from MakerBundle, it created forms that don't have an action attribute. That's fine when the form lives on /voyage/new because no action means it submits back to the current URL. But as soon as we decide to embed our forms on other pages, we need to be responsible and always set the action attribute.
     To do that, open up src/Controller/VoyageController.php. At the bottom, I'll paste in a simple private method. Hit Okay to add the use statement
    
     We can pass a voyage or not... and this creates the form but sets the action. If the voyage has an id, it sets the action to the edit page, else it sets it to the new page.
    
     Thanks to this, up in the new action, we can say this->createVoyageForm($voyage). Copy that... because we need the exact line down in edit
    
     Lovely. Back over, we don't even need to refresh. Open the modal, save and... Ah, that is absolutely lovely! It's submitted and we got the response right back inside the modal. Because... of course! That's the whole point of a Turbo frame. It keeps the navigation inside itself.

--- src/Controller/VoyageController.php
+++ src/Controller/VoyageController.php
@@ -7,6 +7,7 @@ use App\Form\VoyageType;
 use App\Repository\VoyageRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,7 +24,7 @@ class VoyageController extends AbstractController {
 	#[Route('/new', name: 'app_voyage_new', methods: ['GET', 'POST'])]
 	public function new(Request $request, EntityManagerInterface $entityManager): Response {
 		$voyage = new Voyage();
-		$form = $this->createForm(VoyageType::class, $voyage);
+		$form = $this->createVoyageForm($voyage);
 		$form->handleRequest($request);
 
 		if ($form->isSubmitted() && $form->isValid()) {
@@ -50,7 +51,7 @@ class VoyageController extends AbstractController {
 
 	#[Route('/{id}/edit', name: 'app_voyage_edit', methods: ['GET', 'POST'])]
 	public function edit(Request $request, Voyage $voyage, EntityManagerInterface $entityManager): Response {
-		$form = $this->createForm(VoyageType::class, $voyage);
+		$form = $this->createVoyageForm($voyage);
 		$form->handleRequest($request);
 
 		if ($form->isSubmitted() && $form->isValid()) {
@@ -77,5 +78,12 @@ class VoyageController extends AbstractController {
 		}
 
 		return $this->redirectToRoute('app_voyage_index', [], Response::HTTP_SEE_OTHER);
+	}
+
+	private function createVoyageForm(Voyage $voyage = null): FormInterface {
+		$voyage = $voyage ?? new Voyage();
+		return $this->createForm(VoyageType::class, $voyage, [
+			'action' => $voyage->getId() ? $this->generateUrl('app_voyage_edit', ['id' => $voyage->getId()]) : $this->generateUrl('app_voyage_new'),
+		]);
 	}
 }
