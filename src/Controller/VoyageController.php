4c875fdbb2f51a0f5e27d4ac06411a40f25c6c89
petrero <petrero@gmail.com>
2023-12-31 19:26:54 +0200

    24.3 Ok, even though this is working nicely, let's go back to our old way... which was also working nicely. Remove the new Turbo Streams... and undo the code in the controller.
     Tomorrow, we move on to one of my favorite parts of LAST Stack - and the key to organizing your site into reusable chunks: Twig Components.

--- src/Controller/VoyageController.php
+++ src/Controller/VoyageController.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\UX\Turbo\TurboBundle;
 
 #[Route('/voyage')]
 class VoyageController extends AbstractController {
@@ -35,12 +34,11 @@ class VoyageController extends AbstractController {
 			$this->addFlash('success', 'Bon voyage!');
 
 			if ($request->headers->has('turbo-frame')) {
-				$request->setRequestFormat(TurboBundle::STREAM_FORMAT);
-				return $this->renderBlock('voyage/new.html.twig', 'stream_success', [
+				$stream = $this->renderBlockView('voyage/new.html.twig', 'stream_success', [
 					'voyage' => $voyage
 				]);
 
-
+				$this->addFlash('stream', $stream);
 			}
 
 			return $this->redirectToRoute('app_voyage_index', [], Response::HTTP_SEE_OTHER);
