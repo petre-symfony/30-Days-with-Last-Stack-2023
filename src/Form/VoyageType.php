73d7952fd3eafb8ba4f8172afd1d52597adbea77
petrero <petrero@gmail.com>
2024-01-01 15:52:05 +0200

    28.6 Using this is really pleasant. Find the form class: src/Form/VoyageType.php. The library uses decoration. At the top, say $builder equals new DynamicFormBuilder() and pass in $builder.
     This DynamicFormBuilder has the same methods as the original, but one extra: addDependent(). But before we use it, comment-out the 'autocomplete' => true. There's a bug with the autocomplete system and LiveComponents. It should be fixed soon, but I don't want it to get in the way.
    
     Anyway, the addDependent() method takes three arguments. The first is the name of the new field: wormholeUpgrade. The second is an array of fields that this field depends on. In this case, that's only planet. The final argument is a callback function and its first argument will always be a DependentField object. We'll see how that's used in a minute. Then, this will receive the value of every field that it depends on. Because we depend only on planet, the callback will receive that as an argument: ?Planet $planet.
    
     Inside, if we don't have a planet - because the user hasn't selected one yet or the planet is in the Milky Way, just return. And yes, I borked up my space science: I meant for this to be isInOurSolarSystem() - not the milky way. Forgive me Data!
    
     Anyway, because we're returning, there won't be a wormholeUpgrade field at all. Else, add one with $field->add(). This method is identical to the normal add() method except that we don't need to pass the name of the field... because we already pass it earlier. So skip straight to ChoiceType::class... then the options with choices set to an array of "Yes" for true, and "No" for false.
    
     Done! Go check out the result. Refresh, edit and change to a planet that's not in our system. There it is! The field popped into existence! If we go back to a planet that is in our solar system... gone! And... the field saves just fine. When we edit the voyage, the form starts with it. It just works!
    
     Ok, we're nearly at the end of our 30-day journey! Tomorrow, it's time to talk about how we can test our beautiful new frontend features.

--- src/Form/VoyageType.php
+++ src/Form/VoyageType.php
@@ -2,14 +2,19 @@
 
 namespace App\Form;
 
+use App\Entity\Planet;
 use App\Entity\Voyage;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfonycasts\DynamicForms\DependentField;
+use Symfonycasts\DynamicForms\DynamicFormBuilder;
 
 class VoyageType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options): void {
+		$builder = new DynamicFormBuilder($builder);
 		$builder
 			->add('purpose')
 			->add('leaveAt', DateType::class, [
@@ -18,8 +23,21 @@ class VoyageType extends AbstractType {
 			->add('planet', null, [
 				'choice_label' => 'name',
 				'placeholder' => 'Choose a planet',
-				'autocomplete' => true
-			]);
+				//'autocomplete' => true
+			])
+			->addDependent('wormholeUpgrade', ['planet'], function (DependentField $field, ?Planet $planet){
+				if(!$planet || $planet->isInMilkyWay()) {
+					return;
+				}
+
+				$field->add(ChoiceType::class, [
+					'choices' => [
+						'Yes' => true,
+						'No' => false
+					]
+				]);
+			})
+		;
 	}
 
 	public function configureOptions(OptionsResolver $resolver): void {
