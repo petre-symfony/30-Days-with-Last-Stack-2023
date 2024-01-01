fd4d65573e6a8cd461096a57d638371d062f3caf
petrero <petrero@gmail.com>
2024-01-01 15:17:33 +0200

    28.3 LiveComponent & Symfony Forms  - Let's think. After changing any field, I want a Live Component to collect the value of every field and send them to the Live Component system via an Ajax call. The live component will then submit these values into the form object and rerender the template.
     Using Symfony forms with Live Components is a bit more of a complex use-case than the normal case of Live components: where we create some public properties and make them writable.
    
     Fortunately, Live Component ships with a trait to help. In VoyageForm, first, convert this to a Live Component by saying #[AsLiveComponent] then using the DefaultActionTrait.
    
     Next, because we want to bind this component to a form object, use ComponentWithFormTrait. When we do that, we don't need this public form property anymore because that lives inside the trait.
    
     However, this trait does require one new method. Go to Code -> Generate - or Cmd+N on a Mac - and implement the one we need: instantiateForm().
    
     This might look strange at first. But remember, as we change fields in our form, the form values will be sent via Ajax back to our Live component... which then needs to submit them into the form object so it can re-render. This means that, during the Ajax call, our Live Component needs to be able to create our form object. To do that, it calls this method.
    
     To get the logic for this, in VoyageController, all the way at the bottom, copy the guts of createVoyageForm()... then paste them here. Hit okay to add the two use statements.
    
     There's... just one problem: the createForm() and generateUrl() methods don't exist here! But I haven't told you about a crazy, cool thing: live components are Symfony controllers in disguise! And this means we can extend AbstractController.
    
     That's totally allowed and gives us access to all the shortcuts we know and love.
    
     Ok, showtime! Move over. When I type, nothing happens. In this case, LiveComponents waits for the field to change... so it waits for us to move off of the field. As soon as we do, we'll see an Ajax request fire down here. Watch. Boom! See it? That sent the data back, submitted the form and re-rendered the form.
    
     To prove this, clear out the field and hit tab. A validation error! That's coming from Symfony and the normal form validation rendering! Type something again, tab, it goes away. The best part? The planet field down here is also required thanks to Symfony's validation constraints. But the Live Component system is smart: it knows that the user hasn't changed this field yet, so it shouldn't show the validation error. But if we do select a planet... then clear, when it re-renders, it shows the error.

--- src/Twig/Components/VoyageForm.php
+++ src/Twig/Components/VoyageForm.php
@@ -2,10 +2,23 @@
 
 namespace App\Twig\Components;
 
-use Symfony\Component\Form\FormView;
-use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use App\Entity\Voyage;
+use App\Form\VoyageType;
+use Doctrine\DBAL\Driver\Middleware\AbstractConnectionMiddleware;
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
 
-#[AsTwigComponent]
-class VoyageForm {
-	public FormView $form;
+#[AsLiveComponent]
+class VoyageForm extends AbstractConnectionMiddleware {
+	use DefaultActionTrait;
+	use ComponentWithFormTrait;
+
+	protected function instantiateForm(): FormInterface {
+		$voyage = $voyage ?? new Voyage();
+		return $this->createForm(VoyageType::class, $voyage, [
+			'action' => $voyage->getId() ? $this->generateUrl('app_voyage_edit', ['id' => $voyage->getId()]) : $this->generateUrl('app_voyage_new'),
+		]);
+	}
 }
