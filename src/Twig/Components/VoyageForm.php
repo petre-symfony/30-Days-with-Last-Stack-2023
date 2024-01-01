c93f565d2353b77828be3a52e81a9f2201782353
petrero <petrero@gmail.com>
2024-01-01 15:34:40 +0200

    28.4 Passing the Initial Form Data  - This also works fine for the edit form. Hit edit & clear out a field.
     Though, check out instantiateForm(). Hmm, we're always instantiating a new Voyage object: there's never a $voyage variable. We change a field, Live Components sends an Ajax request and, when it creates the form, it does it using a brand new Voyage object, not the existing Voyage object from the database.
    
     And... that's probably okay... because it submits all the data onto it, and it renders correctly.
    
     However, one thing you can do with Live components is submit the form directly into the Component object and handle the save logic there. We're not going to do that, but if we did, the Voyage object bound to the form would always be a new object... and it would always insert a new row into the database.
    
     Passing in the Initial Form Data
     - So even though this works, it's a bit weird.
    
     To tighten this up, we can store the existing Voyage object on the component and use that during form creation. Add a public ?Voyage $initialFormData property. Above this, to make the component system remember this value through all of its Ajax requests, add #[LiveProp].
    
     This is now a non-writable prop that our component will keep track of. And yes, it's non-writable: the user changes the form data directly, not this property. This is just here to help us create the form object on each Ajax call.
    
     Below, change this to $voyage equals $this->initialFormData, else new Voyage().
    
     Finally, pass in the initialFormData by saying :initialFormData="voyage", which is a Twig variable that we already have.
    
     So we won't notice a difference, but when we hit edit and change a field, that Ajax request now creates a Form object bound to this existing Voyage object.
    
     That got a bit technical, but let's zoom out. By rendering out form through a Live Component, we get real-time validation for free! That's cool.

--- src/Twig/Components/VoyageForm.php
+++ src/Twig/Components/VoyageForm.php
@@ -5,18 +5,23 @@ namespace App\Twig\Components;
 use App\Entity\Voyage;
 use App\Form\VoyageType;
 use Doctrine\DBAL\Driver\Middleware\AbstractConnectionMiddleware;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
-class VoyageForm extends AbstractConnectionMiddleware {
+class VoyageForm extends AbstractController {
 	use DefaultActionTrait;
 	use ComponentWithFormTrait;
 
+	#[LiveProp]
+	public ?Voyage $initialFormData = null;
+
 	protected function instantiateForm(): FormInterface {
-		$voyage = $voyage ?? new Voyage();
+		$voyage = $this->initialFormData ?? new Voyage();
 		return $this->createForm(VoyageType::class, $voyage, [
 			'action' => $voyage->getId() ? $this->generateUrl('app_voyage_edit', ['id' => $voyage->getId()]) : $this->generateUrl('app_voyage_new'),
 		]);
