22d34940718beca1e107d3203ffcd0eab501fb20
petrero <petrero@gmail.com>
2024-01-01 14:20:21 +0200

    27.9 Adding a Writable Prop  - One of the key concepts with a Live Component is that you can add a property and allow the user to change that property from the frontend. For example, create a public string $query to represent the search string
     Below, use that when we call the repository
    
     To allow the user to modify this property, we need to give it an attribute: #[LiveProp] with writeable: true
    
     Finally, to bind this property to the input - so that the query property changes as the user types - add data-model="query"
    
     That's it! Check out the result. We start with everything, but when we type... it filters! It even has built-in debouncing.
    
     Backstage, it makes an AJAX request, populates the query property with this string, re-renders the Twig template and pops it right here.
    
     Now that this is working, I don't think we need to load all the results at first. And, look, it's just PHP, so this is easy. If not $this->query, then return an empty array
    
     And in SearchSite.html.twig, add an if statement around this: if this.voyages is not empty, render that... with the endif at the bottom
    
     For those of you that are sticklers for details, yes, with this.voyages, we're calling the method twice. But there are ways around this - and my favorite is called #[ExposeInTemplate]. I won't show it, but it's a quick change.

--- src/Twig/Components/SearchSite.php
+++ src/Twig/Components/SearchSite.php
@@ -5,12 +5,16 @@ namespace App\Twig\Components;
 use App\Entity\Voyage;
 use App\Repository\VoyageRepository;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
 class SearchSite {
 	use DefaultActionTrait;
-	
+
+	#[LiveProp(writable: true)]
+	public string $query = '';
+
 	public function __construct(private VoyageRepository $voyageRepository) {
 		
 	}
@@ -19,6 +23,10 @@ class SearchSite {
 	 * @return Voyage[]
 	 */
 	public function voyages(): array{
-		return $this->voyageRepository->findBySearch(null, [], 10);
+		if(!$this->query) {
+			return [];
+		}
+
+		return $this->voyageRepository->findBySearch($this->query, [], 10);
 	}
 }
